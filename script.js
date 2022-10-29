7d99a1b63b01ec709e92bbb2303debc3e26932dd
evgeniakhatulkova <hatulkova.ev@gmail.com>
2022-10-29 22:34:55 +0200

    creat promt for user's imput and function for full-round of the game

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let playerSelection = prompt("Choose your destiny! Fight!");
+
 // return random number from 1 to 3, which are equivalent to "rock"
 // "paper" or "scissors";
 let randomNumber = Math.floor(Math.random() * 3) + 1;
@@ -10,5 +12,24 @@ function getComputerChoice(randomNumber) {
         return "Scissors"
     }
 }
-let computerHand = getComputerChoice(randomNumber);
-console.log(computerHand);
+let computerSelection = getComputerChoice();
+
+function singleRound(playerSelection, computerSelection) {
+    if(playerSelection === "Rock" && computerSelection === "Paper") {
+        return "You lose! Paper beats Rock!"
+    } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+        return "You win! Rock beats Scissors!"
+    } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+        return "You win! Paper beats Rock!"
+    } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
+        return "You lose! Scissors beats Paper!"
+    } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+        return "You lose! Rock beats Scissors!"
+    } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+        return "You win! Scissors beats Paper!"
+    } else if (playerSelection === computerSelection) {
+        return "It is a tie!"
+    } else {
+        return "You did it wrong"
+    }}
+console.log(singleRound(playerSelection, computerSelection));
